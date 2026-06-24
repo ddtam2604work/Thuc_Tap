0058cd557c23baea141a9c02f9c39f6272e3837a
ddtam2604work <ddtam2604.work@gmail.com>
2026-06-24 09:35:12 +0700

    sửa crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
+import App from './App.jsx';
 import './index.css'; 
 
 // Import 2 Provider mới
-import { NotificationProvider } from './src/context/NotificationContext.jsx';
-import { ConfirmProvider } from './src/context/ConfirmContext.jsx';
+import { NotificationProvider } from './context/NotificationContext.jsx';
+import { ConfirmProvider } from './context/ConfirmContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
